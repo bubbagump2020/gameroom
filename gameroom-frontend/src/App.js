e4fafe340ea5c2fbb1d8343386d501c81fc67de0
Kevin <bubbagump2020@gmail.com>
2019-12-10 12:03:24 -0600

    Adding Redux and UserHomePage

--- gameroom-frontend/src/App.js
+++ gameroom-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom'
 import { SignUp } from './components/SignUp'
 import { Login } from './components/Login';
+import { UserHome } from './components/User/UserHome'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     	<BrowserRouter>
     		<Route exact path='/' component={Login} />
     		<Route exact path='/' component={SignUp} />
+        <Route exact path='/users/:id' component={UserHome} />
     	</BrowserRouter>
     </div>
   );
