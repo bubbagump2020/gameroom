073fbd7f25215816f2a5e24ee36176f391874f2b
Kevin <bubbagump2020@gmail.com>
2019-12-11 15:06:13 -0600

    Adding Character model and basic form

--- gameroom-frontend/src/App.js
+++ gameroom-frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route} from 'react-router-dom'
 import { SignUp } from './components/SignUp'
 import { Login } from './components/Login';
 import { UserHome } from './components/User/UserHome'
-
+import { NewCharacterForm } from './components/Character/NewCharacterForm'
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
     		<Route exact path='/' component={Login} />
     		<Route exact path='/' component={SignUp} />
         <Route exact path='/users/:id' component={UserHome} />
+        <Route exact path='/characters/new' component={NewCharacterForm}/>
     	</BrowserRouter>
     </div>
   );
---
+++ gameroom-frontend/src/components/Character/NewCharacterForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function NewCharacterForm(){
+    return(
+        <div>
+            <h1>New Character Form</h1>
+        </div>
+    )
+}
