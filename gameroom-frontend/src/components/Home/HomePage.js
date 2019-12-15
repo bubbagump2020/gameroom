a41cbb91ef011917ed8ac868ef815f060cd2c7c0
Kevin <bubbagump2020@gmail.com>
2019-12-14 20:32:21 -0600

    Working on the Home Page

--- gameroom-frontend/src/components/Home/HomePage.js
+++ gameroom-frontend/src/components/Home/HomePage.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './HomeStyling/HomePageStyling.scss'
 
 const HomePage = (props) => {
 
     return(
-        <div>
-            <h1>Home page</h1>
+        <div className="grid-container">           
+            <div className="header">
+                <h1>Welcome to the Laughin Halflin Tavern Game Room</h1>
+            </div>
+            <div className="content">
+                <p>
+                    This is a place where you can manage
+                    your characters, campaigns, and your fellow players!
+                </p>
+                <p>
+
+                </p>
+                
+            </div>
+            <div className="sidebar">
+                <p>
+                    some sidebar content
+                </p>
+            </div>
+            <div id="1">1</div>
+            <div id="2">2</div>
+            <div id="3">3</div>
+            <div id="4">4</div>
+            <div id="5">5</div>
+            <div id="6">6</div>
+            <div id="7">7</div>
+            
         </div>
     )
 }
